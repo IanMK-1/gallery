8c1d61677921ce6c82de246845e7f592018b241f
ian mwangi <iankimotho233@gmail.com>
2020-02-28 13:08:24 +0300

    Add Location model

--- gallery/models.py
+++ gallery/models.py
@@ -7,3 +7,9 @@ class Image(models.Model):
     description = models.TextField()
     image_location = models.ForeignKey(Location)
     image_category = models.ForeignKey(Category)
+
+
+class Location(models.Model):
+    location = models.CharField()
+    time_posted = models.DateTimeField(auto_now_add=True)
+
